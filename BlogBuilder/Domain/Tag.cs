e176dcf611dc33b2927033bd4a0115d128c57e22
Kasper B. Graversen <kbilsted@users.noreply.github.com>
2022-12-08 22:32:42 +0100

    GetHashCode is not stable in .Net across executions

--- BlogBuilder/Domain/Tag.cs
+++ BlogBuilder/Domain/Tag.cs
@@ -15,7 +15,7 @@ namespace Kbg.BlogBuilder.Domain
         {
             Value = value;
             DisplayText = Value.Replace('_', ' ');
-            var hashCode = Math.Abs(value.GetHashCode());
+            var hashCode = Math.Abs(GetDeterministicHashCode(value));
             var color = hashCode % MaxValue_ffffff;
             HexCodeForValue = color.ToString("x").PadRight(6, '0');
         }
@@ -33,6 +33,22 @@ namespace Kbg.BlogBuilder.Domain
         public override string ToString()
         {
             return Value;
+        }
+
+        int GetDeterministicHashCode(string str) {
+            unchecked {
+                int hash1 = (5381 << 16) + 5381;
+                int hash2 = hash1;
+
+                for (int i = 0; i < str.Length; i += 2) {
+                    hash1 = ((hash1 << 5) + hash1) ^ str[i];
+                    if (i == str.Length - 1)
+                        break;
+                    hash2 = ((hash2 << 5) + hash2) ^ str[i + 1];
+                }
+
+                return hash1 + (hash2 * 1566083941);
+            }
         }
     }
 }
